2828304da300f228d00ac0641709fb06b512b92b
LuÃís Duarte <sr.zitinho@hotmail.com>
2019-09-29 10:35:08 +0100

    first commit!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarnotaurusV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
