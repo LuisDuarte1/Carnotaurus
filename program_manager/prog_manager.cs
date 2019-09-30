1e35421654f6573e99cf37e7c184e3b18529da8c
LuÃís Duarte <sr.zitinho@hotmail.com>
2019-09-30 23:06:05 +0100

    Initialized Module_Init in prg_manager

--- program_manager/prog_manager.cs
+++ program_manager/prog_manager.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections;
+using NLog;
 
 namespace CarnotaurusV2{
     namespace Program_Manager{
         class Prg_Manager{
 
             private Module_Intercommunication m;
+            private Module_Init m_init;
             private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
             public Prg_Manager(){
                 
                 logger.Info("Initializing Program Manager ");
                 m = new Module_Intercommunication();
+                m_init = new Module_Init(m);
             }
         }
     }   
