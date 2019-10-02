b4483c665291ee4835c1307ae403b3c63db79f65
LuÃís Duarte <sr.zitinho@hotmail.com>
2019-10-02 17:08:16 +0100

    Added overload AddModule to allow for extra args

--- program_manager/module_init.cs
+++ program_manager/module_init.cs
@@ -14,18 +14,37 @@ namespace CarnotaurusV2{
                 m = _m;
             }
 
-            public void AddModule(string class_name){
-                var m1 = new Queue();
-                var m2 = new Queue();
-                var classtype = Type.GetType(class_name);
-                dynamic c = Activator.CreateInstance(classtype);
-                if (classtype.IsSubclassOf(typeof(Module)) is false){
+            public void AddModule(string class_path){
+                Queue m1 = new Queue();
+                Queue m2 = new Queue();
+                var classtype = Type.GetType(class_path);
+                if (classtype.IsSubclassOf(typeof(Module)) is false){ // check if is a subclass of module
                     logger.Warn(String.Format(
                         "Could not add {0} because it dosen't derive from Module", classtype));
-                }
-                c.recv = m1;
-                c.send = m2;
-                
+                    return;
+                } 
+                dynamic c = Activator.CreateInstance(classtype, new object[] {m1, m2}); //Initialize the module
+                m.AddBus(m1, m2);
+                m_list.Add(c);
+
+            }            
+            public void AddModule(string class_path, object[] _args){ //Args respresent addition arguments
+                Queue m1 = new Queue();
+                Queue m2 = new Queue();
+                object[] _args1 = new object[2] {m1,m2};
+                object[] args = new object[_args.Length + _args1.Length];
+                Array.Copy(_args1, args, _args1.Length);
+                Array.Copy(_args, args, _args1.Length);
+                var classtype = Type.GetType(class_path);
+                if (classtype.IsSubclassOf(typeof(Module)) is false){ // check if is a subclass of module
+                    logger.Warn(String.Format(
+                        "Could not add {0} because it dosen't derive from Module", classtype));
+                    return;
+                } 
+                dynamic c = Activator.CreateInstance(classtype, args); //Initialize the module
+                m.AddBus(m1, m2);
+                m_list.Add(c);
+
             }
         }
     }
