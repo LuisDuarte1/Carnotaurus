35f97578b536ee6f3a10fc3ac6844896c1902d4f
LuÃís Duarte <sr.zitinho@hotmail.com>
2019-10-27 10:51:27 +0000

    Improved code readbility on nucleotide

---
+++ program_manager/dna/nucleotide_exceptions.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CarnotaurusV2{
+    namespace Program_Manager{
+        namespace NDNA{
+            class InvalidGeneticInformationException : Exception{
+                public InvalidGeneticInformationException() : base(String.Format("This nucleotide is not valid.")){
+                }
+            }
+        }
+    }
+}
